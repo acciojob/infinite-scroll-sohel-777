08effd9dbd1e11c5639dec7cd685bb2214984548
SK SOHEL RAHAMAN <sohelrahaman43@gmail.com>
2023-06-16 11:11:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const list = document.getElementById("infi-list");
+  let itemCount = 10;
+
+  // Add default list items
+  for (let i = 1; i <= itemCount; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = "List Item " + i;
+    list.appendChild(listItem);
+  }
+
+  // Function to check if user has reached the end of the list
+  function isEndOfList() {
+    const listRect = list.getBoundingClientRect();
+    return listRect.bottom <= window.innerHeight;
+  }
+
+  // Function to add more list items
+  function addMoreItems() {
+    const startIndex = itemCount + 1;
+    const endIndex = itemCount + 2;
+
+    for (let i = startIndex; i <= endIndex; i++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = "List Item " + i;
+      list.appendChild(listItem);
+    }
+
+    itemCount += 2;
+  }
+
+  // Event listener for scrolling
+  window.addEventListener("scroll", function () {
+    if (isEndOfList()) {
+      addMoreItems();
+    }
+  });
+});
 
